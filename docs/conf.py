a307b8efa91f7a9e087f2fd20cd1cae6c6ee6049
Radha Lahoti <75471081+Radha-Lahoti@users.noreply.github.com>
2023-12-27 19:30:55 -0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,13 +16,13 @@ release = 'v0'
 import subprocess, os
 
 def configureDoxyfile(input_dir, output_dir):
-    with open('Doxyfile.in', 'r') as file :
+    with open('Doxyfile', 'r') as file :
         filedata = file.read()
 
     filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
     filedata = filedata.replace('@DOXYGEN_OUTPUT_DIR@', output_dir)
 
-    with open('Doxyfile.in', 'w') as file:
+    with open('Doxyfile', 'w') as file:
         file.write(filedata)
 
 # Check if we're running on Read the Docs' servers
@@ -33,7 +33,7 @@ breathe_projects = {}
 input_dir = '../src'
 output_dir = 'build'
 configureDoxyfile(input_dir, output_dir)
-subprocess.call('doxygen <Doxyfile.in>', shell=True)
+subprocess.call('doxygen', shell=True)
 breathe_projects["DisMech"] = output_dir + '/xml'
 
 extensions = ["sphinx.ext.todo", "sphinx.ext.autodoc", "sphinx.ext.viewcode", "breathe", 'sphinx_mdinclude']
