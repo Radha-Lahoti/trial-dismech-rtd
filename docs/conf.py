367367340182f141e57ab843da5d76c7c9b93f6a
Radha Lahoti <75471081+Radha-Lahoti@users.noreply.github.com>
2023-12-27 18:16:02 -0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,13 +13,6 @@ release = 'v0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-import subprocess, os
-
-read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
-
-if read_the_docs_build:
-
-     subprocess.call('doxygen', shell=True)
 
 extensions = ["sphinx.ext.todo", "sphinx.ext.autodoc", "sphinx.ext.viewcode", "breathe", 'sphinx_mdinclude']
 
