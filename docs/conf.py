917b196ab9ccbe65a11397c2e1d056e8c72007a3
Radha Lahoti <75471081+Radha-Lahoti@users.noreply.github.com>
2023-12-27 18:32:55 -0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,33 +13,11 @@ release = 'v0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-import subprocess, os
-
-def configureDoxyfile(input_dir, output_dir):
-    with open('Doxyfile.in', 'r') as file :
-        filedata = file.read()
-
-    filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
-    filedata = filedata.replace('@DOXYGEN_OUTPUT_DIR@', output_dir)
-
-    with open('Doxyfile', 'w') as file:
-        file.write(filedata)
-
-# Check if we're running on Read the Docs' servers
-read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
-
-breathe_projects = {}
-
-if read_the_docs_build:
-    input_dir = '../'
-    output_dir = 'build'
-    configureDoxyfile(input_dir, output_dir)
-    subprocess.call('doxygen', shell=True)
-    breathe_projects['DisMech'] = output_dir + '/xml'
 
 extensions = ["sphinx.ext.todo", "sphinx.ext.autodoc", "sphinx.ext.viewcode", "breathe", 'sphinx_mdinclude']
 
 # Breathe Configuration
+breathe_projects["DisMech"] = '/xml'
 breathe_default_project = "DisMech"
 breathe_default_members = ('members', 'undoc-members')
 
